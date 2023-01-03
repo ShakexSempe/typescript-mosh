2cf41da5cfae2562c5cf39a0269ada7fd3d79035
Ray-Shakex <bshakex@gmail.com>
2023-01-03 09:54:53 +0200

    tsconfig

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Mosh typescript');
+let age = 31;
+if (age < 50)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log('Mosh typescript');
+
+let age: number = 31;
+if (age < 50) 
+    age += 10;
+    console.log(age);
